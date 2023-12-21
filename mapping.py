e3917daeec04d4dc8a26f5651bdbce5d7e96e298
Veronica Ni <vni1294@gmail.com>
2023-12-21 12:27:43 -0500

    Refactored: used loadenv to test mapping.py in ipython, now removed; city.address does not include city, state, but accesses via relationship to City, so included additional args 'city' 'state'

--- mapping.py
+++ mapping.py
@@ -1,8 +1,6 @@
 """Functions for getting maps to embed for Flask Cafe app."""
 import os
-from dotenv import load_dotenv
-load_dotenv()
-# Found resource to create URL from key/val dict here:
+# Found resource to create URL from params here:
 # https://stackoverflow.com/questions/15799696/how-to-build-urls-in-python-with-the-standard-library
 # https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlencode
 from urllib.parse import urlencode
@@ -11,10 +9,10 @@ GMAPS_API_KEY = os.environ['GMAPS_API_KEY']
 GMAPS_BASE_API_URL = "https://www.google.com/maps/embed/v1/"
 
 
-def get_map_url(address):
+def get_map_url(address, city, state):
     """Get Google Maps URL to embed a map for this location."""
 
-    params = urlencode({'key': GMAPS_API_KEY, 'q': address})
+    params = urlencode({'key': GMAPS_API_KEY, 'q': f"{address} {city} {state}"})
     url = f"{GMAPS_BASE_API_URL}place?{params}"
-    # print('url: ', url)
+
     return url
